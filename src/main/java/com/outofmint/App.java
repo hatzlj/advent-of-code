3240161dd2ad7c39c6e62ae3701641992121e0d6
jhatzl <jakob.hatzl@kmlvision.com>
2023-12-01 14:55:56 +0100

    initial commmit

---
+++ src/main/java/com/outofmint/App.java
@@ -0,0 +1,13 @@
+package com.outofmint;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

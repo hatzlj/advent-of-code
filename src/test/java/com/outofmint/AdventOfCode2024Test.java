6c91b33e222a499a904e4310f738c1a23050748c
Jakob Hatzl <jakob.hatzl@avaisis.biz>
2024-12-13 18:32:44 +0100

    AoC 2024 - day4 part1

--- src/test/java/com/outofmint/AdventOfCode2024Test.java
+++ src/test/java/com/outofmint/AdventOfCode2024Test.java
@@ -1,5 +1,6 @@
 package com.outofmint;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -10,8 +11,13 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -21,6 +27,11 @@ public class AdventOfCode2024Test {
 
     private static final Logger log = LoggerFactory.getLogger(AdventOfCode2024Test.class);
     public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String ANSI_RED_BG = "\u001B[41m";
+    public static final String ANSI_GREEN_BG = "\u001B[42m";
+    public static final String ANSI_YELLOW_BG = "\u001B[43m";
     public static final String ANSI_RESET = "\u001B[0m";
 
 
@@ -257,6 +268,98 @@ public class AdventOfCode2024Test {
         return Stream.of(
                 Arguments.of("/2024/day3-example2.txt", 48)
                 , Arguments.of("/2024/day3-input.txt", 74361272)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    public void test_day4Part1(final String inputSource, final int exXmasCount) {
+        AtomicInteger acXmasCount = new AtomicInteger(0);
+        final String searchString = "xmas";
+        try (BufferedReader r = new BufferedReader(new InputStreamReader(
+                Objects.requireNonNull(AdventOfCode2024Test.class.getResourceAsStream(inputSource))))) {
+            String line;
+            final List<List<Character>> input = new ArrayList<>();
+            while ((line = r.readLine()) != null) {
+                input.add(line.toLowerCase().chars().mapToObj(c -> (char) c).collect(Collectors.toUnmodifiableList()));
+            }
+            final ExecutorService executorService = Executors.newFixedThreadPool(8);
+            final List<CompletableFuture<Void>> futures = new ArrayList<>();
+            for (int row = 0; row < input.size(); row++) {
+                for (int col = 0; col < input.get(row).size(); col++) {
+                    if (input.get(row).get(col) == searchString.charAt(0)) {
+                        final int curR = row;
+                        final int curC = col;
+                        printInput(input, row, col, ANSI_YELLOW_BG);
+                        futures.add(CompletableFuture.runAsync(() -> acXmasCount.getAndUpdate(c -> c + (findAdjacent(input, curR, curC, searchString, -1, 0) ? 1 : 0)), executorService));
+                        futures.add(CompletableFuture.runAsync(() -> acXmasCount.getAndUpdate(c -> c + (findAdjacent(input, curR, curC, searchString, 1, 0) ? 1 : 0)), executorService));
+                        futures.add(CompletableFuture.runAsync(() -> acXmasCount.getAndUpdate(c -> c + (findAdjacent(input, curR, curC, searchString, 0, -1) ? 1 : 0)), executorService));
+                        futures.add(CompletableFuture.runAsync(() -> acXmasCount.getAndUpdate(c -> c + (findAdjacent(input, curR, curC, searchString, 0, 1) ? 1 : 0)), executorService));
+                        futures.add(CompletableFuture.runAsync(() -> acXmasCount.getAndUpdate(c -> c + (findAdjacent(input, curR, curC, searchString, -1, -1) ? 1 : 0)), executorService));
+                        futures.add(CompletableFuture.runAsync(() -> acXmasCount.getAndUpdate(c -> c + (findAdjacent(input, curR, curC, searchString, -1, 1) ? 1 : 0)), executorService));
+                        futures.add(CompletableFuture.runAsync(() -> acXmasCount.getAndUpdate(c -> c + (findAdjacent(input, curR, curC, searchString, 1, -1) ? 1 : 0)), executorService));
+                        futures.add(CompletableFuture.runAsync(() -> acXmasCount.getAndUpdate(c -> c + (findAdjacent(input, curR, curC, searchString, 1, 1) ? 1 : 0)), executorService));
+                    } else {
+                        printInput(input, row, col, ANSI_RED_BG);
+                    }
+                }
+            }
+            CompletableFuture.allOf(futures.toArray(new CompletableFuture[0])).join();
+            executorService.shutdown();
+            assertEquals(exXmasCount, acXmasCount.get());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private boolean findAdjacent(List<List<Character>> input, int startRow, int startCol, String find, int dRow,
+                                 int dCol) {
+        int findIndex = 0;
+        boolean found = false;
+        for (int row = startRow, col = startCol;
+             row >= 0 && row < input.size() && findIndex < find.length() && col >= 0 && col < input.get(0).size();
+             row += dRow, col += dCol) {
+            if (input.get(row).get(col).charValue() == find.charAt(findIndex++)) {
+                printInput(input, row, col, ANSI_GREEN_BG);
+                if (findIndex == find.length()) {
+                    found = true;
+                    break;
+                }
+            } else {
+                break;
+            }
+        }
+        if (found) {
+            log.debug("### +1 ###");
+        }
+        return found;
+    }
+
+    private void printInput(List<List<Character>> input, int rowH, int colH, String color) {
+        StringBuffer output = new StringBuffer();
+        output.append("\n");
+        output.append(String.join("", Collections.nCopies(input.get(0).size(), "=")));
+        output.append("\n");
+        for (int i = 0; i < input.size(); i++) {
+            for (int j = 0; j < input.get(i).size(); j++) {
+                if (i == rowH && j == colH) {
+                    output.append(color).append(input.get(i).get(j)).append(ANSI_RESET);
+                } else {
+                    output.append(input.get(i).get(j));
+                }
+            }
+            output.append("\n");
+        }
+        output.append(String.join("", Collections.nCopies(input.get(0).size(), "=")));
+        output.append("\n");
+        log.debug(output.toString());
+    }
+
+    public static Stream<Arguments> test_day4Part1() {
+        return Stream.of(
+                Arguments.of("/2024/day4-example.txt", 18),
+                Arguments.of("/2024/day4-example2.txt", 4),
+                Arguments.of("/2024/day4-input.txt", 2543)
         );
     }
 }
